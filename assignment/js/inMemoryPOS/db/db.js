24a71299578f8643a307bb41f3b9efd140a1b557
jayani018 <jayaniarunika11@gmail.com>
2023-06-21 20:35:49 +0530

    Created Item Pages

--- assignment/js/inMemoryPOS/db/db.js
+++ assignment/js/inMemoryPOS/db/db.js
@@ -4,9 +4,9 @@ var customerDB = [
     {id: "C00-003", name: "Kavindu Perera", address: "Panadura", salary: 300000}
 ];
 
-// var itemDB = [
-//     {code:"I00-001",description:"Lux",qtyOnHand: 100,unitPrice: 145.00},
-//     {code:"I00-002",description:"Sunlight",qtyOnHand: 150,unitPrice: 345.00},
-//     {code:"I00-003",description:"Light Boy",qtyOnHand: 400,unitPrice: 245.00}
-// ];
+var itemDB = [
+    {code:"I00-001",name:"Lux",qtyOnHand: 100,unitPrice: 145.00},
+    {code:"I00-002",name:"Sunlight",qtyOnHand: 150,unitPrice: 345.00},
+    {code:"I00-003",name:"Light Boy",qtyOnHand: 400,unitPrice: 245.00}
+];
 // var orderDB = [];
